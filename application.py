fc945d22d7dff4fbd8e33c067bdd8df51c740b0c
LQZ <875751019@qq.com>
2020-09-11 18:16:07 +0800

    新闻-增删改查

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-app.secret_key = os.urandom(24)
+app.secret_key = "11111111"
 
 app.config.from_object('configs.default-setting')
 
--- controllers/admin.py
+++ controllers/admin.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from models.User import User
+from models.Article import Article
 from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
-from common.forms import UserForm
+from common.forms import UserForm, ArticleForm, ModifyForm
 from application import db
+from datetime import datetime
 
 admin_route = Blueprint('admin', __name__)
 
@@ -50,33 +52,96 @@ def logout():
     return redirect(url_for(".login"))
 
 
+
 # 新闻列表
 @admin_route.route('/article/')
 @admin_route.route('/article/<int:page>')
 @admin_login_require
 def article_index(page=None):
-    return render_template('/admin/article/index.html')
+    if page is None:
+        page = 1
+    keyword = request.args.get('search')
+    if keyword:
+        articles = Article.query.filter(Article.title.contains(keyword)).order_by(Article.id).paginate(page, per_page=5)
+
+        condition = "?search=" + keyword
+
+        return render_template('/admin/article/index.html', articles=articles, condition=condition)
+    else:
+        articles = Article.query.order_by(Article.id).paginate(page, per_page=5)
+        return render_template('/admin/article/index.html', articles=articles)
 
 
 # 新增新闻
 @admin_route.route('/article/add', methods=['GET', 'POST'])
 @admin_login_require
 def article_add():
-    return render_template('/admin/article/add.html')
+    form = ArticleForm()
+
+    if form.validate_on_submit():
+        try:
+            print(form.content)
+            article = Article(
+                title=form.title.data,
+                content=form.content.data,
+                types=form.types.data,
+                img_url=form.img_url.data,
+                author=form.author.data,
+                is_recommend=form.is_recommend.data,
+                is_valid=form.is_valid.data,
+                created_at=datetime.now()
+            )
+            db.session.add(article)
+            db.session.commit()
+            flash("添加新闻成功！")
+            return redirect(url_for('.article_index'))
+        except:
+            flash("添加新闻失败!", category="error")
+    return render_template('/admin/article/add.html', form=form)
 
 
 # 编辑新闻
 @admin_route.route('/article/edit/<int:pk>', methods=['GET', 'POST'])
 @admin_login_require
 def article_edit(pk):
-    return render_template('/admin/article/edit.html')
+    article = Article.query.get(pk)
+    if article is None:
+        return redirect(url_for('.article_index'))
+    form = ArticleForm(obj=article)
+    print(form.content)
+    if form.validate_on_submit():
+        try:
+            article.title = form.title.data
+            article.content = form.content.data
+            article.types = form.types.data
+            article.img_url = form.img_url.data
+            article.author = form.author.data
+            article.is_recommend = form.is_recommend.data
+            article.is_valid = form.is_valid.data
+            article.created_at = datetime.now()
+            db.session.add(article)
+            db.session.commit()
+            flash("新闻编辑成功!")
+            return redirect(url_for('.article_index'))
+        except:
+            flash("新闻编辑失败!", category="error")
+    return render_template('/admin/article/edit.html', form=form)
 
 
 # 删除单个新闻
 @admin_route.route('/article/delete/<int:pk>')
 @admin_login_require
 def article_delete(pk):
-    pass
+    article = Article.query.get(pk)
+    if article is None:
+        return redirect(url_for(".article_index"))
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        flash("删除新闻成功!")
+    except:
+        flash("删除新闻失败!", category="error")
+    return redirect(url_for(".article_index"))
 
 
 # 管理员列表
@@ -88,11 +153,11 @@ def user_index(page=None):
         page = 1
     keyword = request.args.get("search")
     if keyword:
-        users = User.query.filter(User.username.contains(keyword)).order_by(User.id).paginate(page, per_page=1)
+        users = User.query.filter(User.username.contains(keyword)).order_by(User.id).paginate(page, per_page=5)
         condition = "?search=" + keyword
         return render_template('/admin/user/index.html', users=users, condition=condition)
     else:
-        users = User.query.order_by(User.username).paginate(page=page, per_page=1)
+        users = User.query.order_by(User.id).paginate(page=page, per_page=5)
         return render_template('/admin/user/index.html', users=users)
 
 
